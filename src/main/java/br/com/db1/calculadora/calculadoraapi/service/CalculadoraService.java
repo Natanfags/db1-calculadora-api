9fdaa50ac09758aa1e8a2bba22f9debaa9564edc
Natan Fagundes <natan.fags@gmail.com>
2018-12-12 16:25:33 -0200

    criado calculadora services

--- src/main/java/br/com/db1/calculadora/calculadoraapi/service/CalculadoraService.java
+++ src/main/java/br/com/db1/calculadora/calculadoraapi/service/CalculadoraService.java
@@ -1,11 +1,21 @@
 package br.com.db1.calculadora.calculadoraapi.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.db1.calculadora.calculadoraapi.model.Calculadora;
+import br.com.db1.calculadora.calculadoraapi.repository.CalculadoraRepository;
 
 @Service
 public class CalculadoraService {
 	
 	@Autowired
-	private 
+	private CalculadoraRepository calculadoraRepository;
+	
+	public Calculadora salvaResultado(Double numeroUm, Double numeroDois) {
+		Calculadora calculadora = new Calculadora(numeroUm, numeroDois);
+		calculadoraRepository.save(calculadora);
+		return calculadora;
+	}
 
 }
