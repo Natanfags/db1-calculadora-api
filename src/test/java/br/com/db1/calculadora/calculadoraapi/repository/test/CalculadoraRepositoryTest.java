fdfb7606efe98e192c524800870e1221b594fe86
Natan Fagundes <natan.fags@gmail.com>
2018-12-12 17:49:41 -0200

    stardado implementacao dos testes

--- src/test/java/br/com/db1/calculadora/calculadoraapi/repository/test/CalculadoraRepositoryTest.java
+++ src/test/java/br/com/db1/calculadora/calculadoraapi/repository/test/CalculadoraRepositoryTest.java
@@ -1,5 +1,34 @@
 package br.com.db1.calculadora.calculadoraapi.repository.test;
 
-public class CalculadoraRepositoryTest {
+import java.util.List;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.db1.calculadora.calculadoraapi.model.Calculadora;
+import br.com.db1.calculadora.calculadoraapi.model.Operacao;
+import br.com.db1.calculadora.calculadoraapi.repository.CalculadoraRepository;
+
+public class CalculadoraRepositoryTest {
+	
+	@Autowired
+	private CalculadoraRepository calculadoraRepository;
+	
+	@After
+	public void after() {
+		calculadoraRepository.deleteAll();
+	}
+	
+	@Test
+	public void deveSalvarUmResultado() {
+		Calculadora calculadora = new Calculadora(10.0, 10.0, Operacao.SOMA);
+		calculadoraRepository.save(calculadora);
+		
+		List<Calculadora> calculadoras = calculadoraRepository.findAll();
+		
+		Calculadora resultadoSalvo = calculadoras.get(0);
+		Assert.assertEquals(calculadora.getResultado(), resultadoSalvo.getResultado());
+	}
 }
